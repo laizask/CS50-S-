0abc1a4ffe4d168163a0cbcf7b9347f3904965a2
laiza barbosa <146351860+laizask@users.noreply.github.com>
2024-04-11 11:26:30 -0300

    Create readability.c

---
+++ readability.c
@@ -0,0 +1,91 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+int coleman_liau_index(int letters, int words, int sentences);
+
+int main(void)
+{
+    string text = get_string("Texto: ");
+
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
+
+    int index = coleman_liau_index(letters, words, sentences);
+
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
+    return 0;
+}
+
+int count_letters(string text)
+{
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int count_words(string text)
+{
+    if (strlen(text) == 0)
+    {
+        return 0;
+    }
+
+    int count = 1;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isspace(text[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int count_sentences(string text)
+{
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int coleman_liau_index(int letters, int words, int sentences)
+{
+    float L = (float) letters / words * 100;
+    float S = (float) sentences / words * 100;
+
+    float index = 0.0588 * L - 0.296 * S - 15.8;
+
+    int rounded_index = round(index);
+
+    return rounded_index;
+}
